851f98aee52ede2820aa885143eac5a9e4e61538
Nick-Hericks <nickhericks@gmail.com>
2019-04-20 16:23:20 +0200

    Create renderStars function

--- src/components/StarRating.js
+++ src/components/StarRating.js
@@ -3,20 +3,40 @@ import Star from './Star';
 
 class StarRating extends Component {
 
-  // Initialize a 'rating' state
+  // Each instance of starRating component will manage it's own state
+	state = {
+		rating: 0
+	};
 
-
-  // Write a function that returns 5 Star components
-
+	// Function to return 5 Star components
+	// Needed to create this here because for loops are 
+	// not allowed within the render() method
+	renderStars = () => {
+		let stars = [];
+		let maxRating = 5;
+		for(let i = 0; i < maxRating; i++) {
+			stars.push(
+				<Star 
+					key={i}
+					updateStars={this.updateStars}
+				/>
+			);
+		}
+		return stars;
+	}
 
   // Write an event handler that updates the rating state.
   // Pass the function to a Star component via props
+updateStars = () => {
 
+
+}
 
   render() {
     return (
       <ul className="course--stars">
-        {/* Render the Star components */}
+				{/* Render 5 star component */}
+				{ this.renderStars() }
       </ul>
     );
   }
