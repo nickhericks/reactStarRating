45fd9a5df124a4706a032602151344194db2d453
Nick-Hericks <nickhericks@gmail.com>
2019-04-20 15:44:13 +0200

    Create App.js class component

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Course from './Course';
 
-const App = (props) => {
-  return (
-    <div className="container">
-      {props.courses.map( course => 
-        <Course
-          {...course}
-          key={course.id}
-        />      
-      )}
-    </div>
-  );
+class App extends Component {
+
+
+
+
+
+
+	render() {
+		return (
+			<div className="container">
+				{this.props.courses.map( course => 
+					<Course
+						{...course}
+						key={course.id}
+					/>      
+				)}
+			</div>
+		);
+	}
 }
 
 export default App;
