59cc34671a11009fa725ea0aedebed45fad7266d
Nick-Hericks <nickhericks@gmail.com>
2019-05-06 16:24:35 +0200

    Deploy to gh-pages

---
+++ build/precache-manifest.d06f91c86fa8f6a01aa4b96aca91db9d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f33484aa81a58235e400",
+    "url": "/reactStarRating/static/css/main.96f2c465.chunk.css"
+  },
+  {
+    "revision": "f33484aa81a58235e400",
+    "url": "/reactStarRating/static/js/main.f33484aa.chunk.js"
+  },
+  {
+    "revision": "0cd6058fc3c24877ba89",
+    "url": "/reactStarRating/static/js/1.0cd6058f.chunk.js"
+  },
+  {
+    "revision": "710e752dc2da0fe6c0c1",
+    "url": "/reactStarRating/static/js/runtime~main.710e752d.js"
+  },
+  {
+    "revision": "3a4be45af1f2810f3daec0e7215682a1",
+    "url": "/reactStarRating/index.html"
+  }
+];
